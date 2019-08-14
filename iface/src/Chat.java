84a08d5c5f9ab249793c6038fec09fbe7b329371
Daniel de Albuquerque Diniz <42755139+danie98diniz@users.noreply.github.com>
2019-08-14 20:43:07 -0300

    Update Chat.java

--- iface/src/Chat.java
+++ iface/src/Chat.java
@@ -5,6 +5,17 @@ public class Chat {
     private ArrayList<String> Messages = new ArrayList<>(); // list of messages in chat
 
 
+    public String GetWhoYouTalkingTo(Chat YourChat, Account YourAccount){
+        String Username;
+        if(YourChat.getChatMembers().get(0).getUsername() .equals(YourAccount.getUsername())){
+            Username = YourChat.getChatMembers().get(1).getUsername();
+        }
+        else{
+            Username = YourChat.getChatMembers().get(0).getUsername();
+        }
+        return Username;
+    }
+
     public void AddMember (Account AccountToBeAdded){ // add a account to the members list
         this.ChatMembers.add(AccountToBeAdded);
     }
@@ -20,6 +31,20 @@ public class Chat {
     public void setChatMembers(ArrayList<Account> chatMembers) {
         ChatMembers = chatMembers;
     }
+
+    //---------------------------------------------  ADDING A CHAT  --------------------------------------------------------
+    public void CreateANewChat(Account YourAccount, Account HisAccount){
+        Chat new_chat = new Chat();
+        new_chat.AddMember(YourAccount);
+        new_chat.AddMember(HisAccount);
+        YourAccount.AddChatToAccount(new_chat);
+        HisAccount.AddChatToAccount(new_chat);
+        System.out.println("You can now chat with "+HisAccount.getUsername()+"!");
+        System.out.println("Go to 'your chats' to begin a conversation");
+    }
+
+
+
 }
 
 
